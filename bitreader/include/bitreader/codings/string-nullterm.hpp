c6e902067e04d2192a70958d2000e04965e8ef2d
Andrey Kaydalov <sorsarre@gmail.com>
2018-11-28 01:57:35 +0300

    Add null-terminated string binary codec

---
+++ bitreader/include/bitreader/codings/string-nullterm.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <bitreader/bitreader-utils.hpp>
+
+namespace brcpp::ext
+{
+    struct string_nullterm: public brcpp::binary_codec
+    {
+        using value_type = std::string;
+
+        template<typename Reader>
+        static std::string read(Reader& br)
+        {
+            std::string ret;
+            char current = br.template read<char>(8);
+            while (current != '\0') {
+                ret.push_back(current);
+                current = br.template read<char>(8);
+            }
+
+            return ret;
+        }
+
+        template<typename Writer>
+        static void write(Writer& w, const std::string& value)
+        {
+            for (char c: value) {
+                w.write(c, 8);
+            }
+            w.write('\0', 8);
+        }
+    };
+}
